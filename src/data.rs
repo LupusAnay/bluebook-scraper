e1deea1bd386bd2b8e167f0532f34e93b3f17e1e
Ilya Kopeshtianski <lupusanay@gmail.com>
2020-05-25 19:56:31 +0300

    Add annual vol to CompanyInfo

--- src/data.rs
+++ src/data.rs
@@ -13,6 +13,7 @@ pub struct CompanyInfo {
     pub size: Option<String>,
     pub duns: Option<String>,
     pub other: Vec<String>,
+    pub annual_vol: Option<String>,
 }
 
 #[derive(Debug, PartialEq)]
@@ -28,9 +29,37 @@ pub struct ServiceArea {
     pub cities: Vec<String>,
 }
 
+#[derive(Debug, PartialEq)]
+pub struct Brand {
+    pub logo: String,
+    pub name: String,
+}
+
 pub type Category = String;
 
-pub type Project = String;
+#[derive(Debug, PartialEq)]
+pub struct ProjectExperience {
+    pub general: Vec<String>,
+    pub commercial: Vec<String>,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Qualification {
+    name: String,
+    issuer: Option<String>,
+    qualifier: Option<String>,
+    expiration: Option<String>
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Project {
+    name: String,
+    image: Option<ImageLink>,
+    location: Option<String>,
+    completed: Option<bool>
+}
+
+pub type ImageLink = String;
 
 #[derive(Debug, PartialEq)]
 pub struct CompanyProfile {
@@ -40,6 +69,12 @@ pub struct CompanyProfile {
     pub story: String,
     pub categories: Vec<Category>,
     pub service_areas: Vec<ServiceArea>,
-    pub project_experience: Vec<Project>,
+    pub project_experience: ProjectExperience,
     pub emails: Vec<String>,
+    pub logo: Option<ImageLink>,
+    pub preferred_brands: Vec<Brand>,
+    pub social_media: Vec<String>,
+    pub qualifications: Vec<Qualification>,
+    pub media_gallery: Vec<ImageLink>,
+    pub projects: Vec<Project>
 }
